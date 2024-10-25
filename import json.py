9485380f7fb09cf5a7771aabc36b27dcc47b3af5
indahputri24 <indahptr401@gmail.com>
2024-10-25 21:14:28 +0800

    kirei
    kirei

--- import json.py
+++ import json.py
@@ -1,6 +1,6 @@
 import json
 from prettytable import PrettyTable
-
+print('☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬ ☬')
 user = {
     'admin': {'password': 'group1best', 'role': 'admin'}
 }
@@ -31,7 +31,26 @@ def menu_admin():
         else:
             print('Pilihan Tidak Tersedia')
 
+table = PrettyTable()
+#Baris ini menentukan nama-nama kolom untuk tabel (field names)
+table.field_names = ['Nama Game', 'Tanggal Rilis Game', 'Pengembang Game', 'Genre Game', 'Deskripsi Game'] #
+def game(nama, rilis, pengembang, genre, deskripsi): #Mendefinisikan fungsi bernama paket
+    table.add_row([nama, rilis, pengembang, genre, deskripsi]) #Didalam fungsi paket, baris ini menambahkan sebuah baris baru ke dalam tabel
+
 def tambah_game():
-    nama = input
-    harga = 
-    genre = 
+    while True:
+        nama = input('Masukkan Nama Game: ')
+        rilis = input('Masukkan Tanggal Rilis Game: ')
+        pengembang = input('Masukkan Pengembang Game: ')
+        genre = input('Masukkan Genre Game: ')
+        deskripsi = input('Masukkan Deskripsi Game: ')
+        game(nama, rilis, pengembang, genre, deskripsi)
+        print('==============================================')
+        print(f'Game dengan Nama {nama} Berhasil Ditambahkan')
+        print('----------------------------------------------')
+        pilihan = input('Apakah Ingin Menambah Paket Lagi (YA/TIDAK): ') #Perulangan pilihan
+        if pilihan == 'TIDAK':
+            break
+
+def lihat_game():
+    
